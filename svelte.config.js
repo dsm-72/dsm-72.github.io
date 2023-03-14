efd827d047bbb1a8f15701e668db5be750d45360
dsm-72 <100158155+dsm-72@users.noreply.github.com>
2023-03-14 09:04:59 -0400

    adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
+	// NOTE: I did this from https://javascript.plainenglish.io/sveltekit-github-pages-4fe2844773de
 };
 
 export default config;

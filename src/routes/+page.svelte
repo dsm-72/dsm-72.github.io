c5ced720e7bc66eb92d5704043f29485f609a76e
dsm72 <sumner.magruder@yale.edu>
2023-03-16 19:41:20 -0400

    last

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,17 +5,45 @@
     $: datasets = data?.datasets
 </script>
 
-<div class="bg-slate-300">
-{#await collaborators}
-    waiting
-{:then results} 
-{#each results as collaborator, i (i)}
-    <div>
-        {collaborator}        
+<!-- <div class="bg-slate-100 flex place-content-center">
+    <div class="prose">
+        {#await collaborators}
+            waiting
+        {:then results} 
+        {#each results as {image, name:{first, last}, lab}, i (i)}
+        <figure class="
+            md:flex bg-slate-100 rounded-xl p-8 
+            md:p-0 dark:bg-slate-800 
+            "
+        >
+            <img class="
+                w-24 h-24 md:w-48 md:h-auto md:rounded-none 
+                rounded-full mx-auto 
+                " 
+                src="https://unsplash.it/400/600" 
+                alt="{image}" 
+            >
+            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+              <blockquote>
+                <p class="text-lg font-medium">
+                  ...
+                </p>
+              </blockquote>
+              <figcaption class="font-medium">
+                <div class="text-sky-500 dark:text-sky-400">
+                  {first} {last}
+                </div>
+                <div class="text-slate-700 dark:text-slate-500">
+                  {lab}
+                </div>
+              </figcaption>
+            </div>
+          </figure>
+        {/each}
+        {/await}
     </div>
-{/each}
-{/await}
-</div>
+</div> -->
+
 
 <!-- <div class="prose">
 {#each data?.collaborators as collaborator}

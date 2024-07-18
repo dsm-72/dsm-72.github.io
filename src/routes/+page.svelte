869912bf907ad78a40524cf9ce8b141986bf10cf
dsm-72 <sumner.magruder@yale.edu>
2024-07-18 09:11:51 +0200

    tiny

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,172 @@
     $: datasets = data?.datasets
 </script>
 
+<div class="flex place-content-center bg-white select-none pointer-events-none cursor-default text-xs overflow-clip disabled">
+  <div class="prose text-white select-none pointer-events-none cursor-default text-xs overflow-clip hidden disabled">
+    # Terms and Conditions
+
+    ## Regis Recapture
+    Art, albeit subjective, is herein acknowledged as a body of work which 'moves' those experiencing it, as in a piece of art impresses itself into the audience. Regis Recapture Regency - 'Regis' and 'R3' for short - is a performative art project which impressing itself onto artist, performer(s) (if different than the artist), and audience through appropriation of their time and more forcefully through allocation of a binding legal agreement between the audience and artist. In this agreement the signing party, an audience member, registers for an 'account' with the synthetic intelligence (SI) Regis recommender system (RecSys). From the perspective of Regis, the 'account' and audience member are synonymous and so to within the agreement will the word account be used to refer to the signing audience member with intention of obfuscating the terms of agreement. From the perspective of Regis, all non-Regis parties e.g. audience regardless of whether or not they are in attendance, accounts, non-accounts, etc are considered 'bots' - actors whose actions are with an agenda detrimental to Regis's recommendations - unless verified. Therefore, at the start of the R3 initiative, Regis is the sole verified agent in the world: from hereon referred to as 'platform'. Let it be stated clearly that the artist behind R3 may be referred to as 'W0' in some instances. Thus any agreement signed by an audience member and the performative 'Regis', artist, W0, to register the audience member and in so doing establish them formally as an account, is in accordance with R3 initiative, synonymous.
+    
+    What then are the goals of the R3 initiative ('#Recapture') for which such lengths, such as legal agreements, are employees? In no particular order
+    
+    - Raise awareness for the undo power recommender systems have on human society,
+    
+    - Emphasize the ridiculous state of regulation regarding social media,
+    
+    - Advocate for technology literacy ('tech.lit.')  amongst the public,
+    
+    - Attempt to produce approachable tech.lit. outreach for the public,
+    
+    - Prod governing bodies into action regarding regulation,
+    
+    - Push the boundaries of regulation in part by secondary and tertiary effects,
+    
+    - Facilitate discourse regarding regulatory policy,
+    
+    - Encourage higher education through the questioning of the academia's perceived falling value proposition, and
+    
+    - Inspire artists to use their talent to break through complexity of the world and connect with one another on a human level.
+    
+    Implicit in the above statements and this agreement is the questionable nature of having every day people frequently sign a lengthy legal document to use a platform / app, which may included questionable practices in the terms and conditions hidden through 'legalese'. While not a requirement for life, these platforms have been ever more incorporated into society. Even if one were unaware of the common joke that most people do not read the terms, many of these platforms now provide a button which scrolls the text of the terms to the bottom, which then allowing new people (users)  to sign an agreement under the premise that they are informed to what they are signing: although they may have 'read' the terms and conditions in about milliseconds; this is before the seemingly lack of accountability for the companies that blatantly breach their own terms maliciously.
+    
+    So too will R3 satire this absurdity with a more engaging and enriching proposition; namely those audience members who register for an account on Regis's platform do so with the core machination to 'recapture' all rights, data, permissions, etc that may have been signed away to other platforms. In order to maximize success of the R3 project, naturally some terms and conditions apply.
+    
+    Foremost, all text herein is to be taken in 'good faith' and this agreement is both binding and perpetually promissory. What does that mean?
+    
+    Language is ambiguous. Even documents which shoulder societies require interpretation. Consider constitutional scholars who debate the both the word as writ and authors' intents. To this end R3 provides "Geist des Gesetzes" (GdG) or the "Spirit of the Law" (SL). In pursuit of clarity, the Geist des Gesetze provides a means of outlining examples expounding  how to interpret what is written in the bound agreement.  
+    
+    While we will cover GdG later, in regards to 'good faith,' it means forgiveness for minutiae that may be at odds or puts the legality of this agreement into question. Towards that later point, 'perpetually promissory' means that the signee not only agrees with the terms and conditions outlined here, and whether struck down even or expanded on with updates (e.g. more examples in the GdG), the signing party swears to all relevant parties that they accept and sign the terms and conditions of the revision / renewal. In other words, in good faith the signing party agrees to core tenants and ethos of R3 regardless of what form it takes (similar in concept to a 'living' document). 
+    
+    Although leveraging 'dark patterns' and producing a truly satirical terms and conditions is part of the R3 project, R3 is not a malicious enterprise. To that end the core tenants and ethos of R3 of which the signing party agrees to (with the perpetual promissory protocol in place) are as follows. See the GdG's section Kernel Clarity for justifications and rational.
+    
+    ## Core Tenants:
+    The signing party hereby agrees that:
+    
+    • the signing party is not just literate, but understands the document they are signing,
+    
+    • the signing party was given sufficient time to read and review this agreement before signing,
+    
+    • this agreement is non-coerced,
+    
+    • the signing party were permitted to ask questions with Regis prior to signing,
+    
+    • the terms and conditions of R3 are superseding to all preexisting agreements with totality and are absolute at the time of signing,
+    
+    • this arrangement is perpetually promissory and is written in 'good faith' as outlined above,
+    
+    • the 'assurance of aliases' holds,§
+    
+    • waives requirements of notary and witnesses,§
+    
+    • the *in vivo* anatomic gift of ownership of their heart is consensual, valid, and explicit,§
+    
+    • W0 / Regis / R3 may claim they "captured" the signing party's heart both literally and proverbially as part of promotion of the R3 project,
+    
+    • the Geist des Gesetze, as part of this agreement, should be consulted which determining the legal standing of this agreement or its sworn accepted legally adherent revision under the perptual promissary protocol,
+    
+    • this agreement constitutes their realization *per publicum* and conferment of the following degrees: doctorate of fine arts (DFA), doctorate of public policy (DPP), doctorate of philosophy (PhD) for marketing, and a doctorate of divinity (DDiv),§
+    
+    • this agreement not only authorization but prefers the use of these accolades - controversial they might be - in the marketing and pursuit of success when deemed favorable by W0,§
+    
+    • in the even of nullification, perpetual promissory protocol included, that as testament to the impact this R3 project had will be the withstanding records in perpetuity,§
+    
+    • regardless of W0's express wishes on this matter, the recognition of W0 as Emperor for their sand Chief Technocrat of the sovereign state of Tera Technocracy, where title of emperor refers to both the Technocracy and W0 as having the self-sovereign status,§
+    
+    • the assignment of 'Royal Fool' is recommended without peer for W0 to be at the service of Her Grace and Royal Highness Ingrid Alexandra,§
+    
+    • it is the express and heartfelt desire for W0 to pursue audience with Her Royal Highness as part of the R3 project and terms of release,§
+    
+    • the signing party may be releasee for the perpetual promissory protocol provided an ever expandable option from the section Terms of Release in the GdG is achieved or at the uncoerced, free will of W0. Further there is at least one release option present at signing,
+    
+    • this agreement includes surrendering all legal recourse of action by the signing party,§
+    
+    • W0, especially whilst status of sovereignty is non-reciprocal with the legal body, lacks all legal liability,§
+    
+    • in the event of legal recourse in an attempt to nullify this agreement, each core tenant designated with a trailing § must be treated as stand alone agreements (bundled here for convenience and reduced text),
+    
+    • it is the explicit opinion of the signing part that W0 is not to be held in contempt, accused of falsification, academic dishonesty, academic fraud, fraud, identity theft, conspiracy, resurgence, resurrection, etc for the accolades the signing party unilaterally imposes on W0 and encourages their use of (see GdG),
+    
+    • regarding GDPR this (1) constitutes consent; (2) sufficient information regarding data usage, access, portability, deletion / rights to be forgotten; (3) the signee waives the provisions of GDPR in regards to (2) when they (the signing party) do not provide information to satisfy (2); (4) explicitly hold 3rd party vendors used by R3 responsibly for all violations of data protection; and (5) that the GdG satisfies the need of privacy policy,
+    
+    • this constitutes, conveys, and bestows power of attorney to W0, 
+    
+    • should power of attorney be to W0 be revoked, this constitutes explicitly permission to pursue on the signing party's behalf, entities that produce material which influence the signing party's choices regardless of degree of abstraction (secondary, tertiary, etc),
+    
+    • any document or form provided as part of the R3 project with the intent of showing support for R3 serves as legally binding to this agreement; more explicitly, a checkbox that states "I hereby agree" is deemed minimally acceptable in lieu of this form,
+    
+    • in order to reduce digital and physical footprint of the R3 project, these terms need not be present alongside the signature / agreement to be valid,
+    
+    • in order to be satirical, the R3 project may make these terms convoluted to find, read, or otherwise process,
+    
+    • W0 and the R3 project are not liable should the signing party refer another user under false pretenses, 
+    
+    • the signing party agrees all subsequent / new accounts (e.g. through *de novo* creation, referral, etc) opened by the signing party are implicitly bound to this agreement,
+    
+    • perceived differences of 'good faith' do not constitute annulment,
+    
+    • an explicit definitions section may be added post signage in accordance with the GdG and R3 aim for convenience,
+    
+    • typos are not invalidating,
+    
+    • regardless of current view of the R3 initiative, W0 and R3 initiative may used the fact that it was agreed too indefinitely (see GdG),
+    
+    • the signing party agrees that by agreeing to R3 they are implicitly signing and agreeing to each tenant ending with an § above in addition as a form of redundancy,
+    
+    • recommendations are only given to verified accounts, 
+    
+    • account verification requires submission of ID,
+    
+    • the signing party is liable for the cost of recommendations (electric, carbon, etc) to be paid in full upfront,
+    
+    and
+    
+    • the Geist des Gesetzes is considered sufficient at the point of signing to understand.
+    
+    
+    # Geist Des Gesetzes
+    
+    ## Terms of Release
+    Per the core tenants of the R3 initiative which the signing party has agreed, should the signing party seek release, they may be released upon request in the event that all conditions for one of these options are met.
+    
+    **Option 1**
+    - 1 hour private audience with the Her Royal Highness the Princess,
+    - formal appointment of Royal Fool,
+    - letter of introduction to the Pope, and
+    - Norwegian citizenship granted.
+    
+    **Option 2**
+    - Marriage to Her Royal Highness the Princess,
+    - funds for marriage ceremony reappropriated to charity, and
+    - upon annulment 1 hour national interview
+    
+    **Option 3**
+    - citizenship granted, and
+    - non-honorary conferment of two degrees listed in the core terms by two universities in Norway.
+    
+    ## Heart of the Matter
+    Agreement of R3 constitutes, in part, an *in vivo* anatomical gift from the account. Specifically, ownership of the account's heart is gifted to the agent W0 in totality and barring liability. From hereon this gift is referred to as "account thread" (AT). The account is liable for all maintenance, operations, and processes of the AT ("allocation"). Upon closing an account, the AT may be physically migrated to another. Ownership remains with agent W0 in perpetuity, across accounts, including implicit ownership of child accounts' ATs. W0, as owner of the AT, may at their leisure and on behalf of the account pursue improvements to maintenance, operation, and processes of the AT. 
+    
+    
+    ## Account Threads
+    What does "ownership of the account's heart is gifted to the agent W0 in totality and barring liability" actually mean, *in spiritu*? To answer this, let us first build intuition by considering the day-to-day of an account with W0 ownership of the AT. In essence, everything is the same. After all, the gift is *in vivo* and it is up to the account to use the allocated AT as they wish, without W0 being liable for the accounts actions. 
+    
+    Why gift the AT then? Consider all the factors outside of the account's control that can impact the AT for which they are held liable. For example, the account is liable for maintenance. While the energy requirements of the AT can vary for a variety of reasons, for simplicity let us consider solely energy acquisition. The sources of energy an account has access to is geo-blocked. There is a duration, at any time, that an AT can function on reserves without energy input. As account activity is a serious drain on energy – amongst other account resources - for any point of time there is a not only a fix duration before forced account forfeiture, by extension a fixed distance an account can travel in pursuit of energy income. Further not all energy sources are the same. Decidedly some energy sources are referred to colloquially as "clean". 
+    
+    While W0 is not liable for energy acquisition on behalf of the account, or even energy accessibility, as owner of the AT, it is of W0's interest to ensure energy sources are available, and especially that those available are non-corrosive. Although the account has independent action, including liability for the AT's use, as owner W0 may take legal action against third parties which persuade, pursue, mislead, or otherwise make deleterious energy sources favorable for the account (leading them to choose to take them).
+    
+    In other-words the account has free will and right of choice within these terms and agreement, whereas W0, as owner, has legal right to pursue 3rd parties that influence those choices to the detriment of W0's property, and the accounts allocated AT.
+    
+    Furthermore, the AT is granted in "totality" this includes both literal and metaphorical uses of the AT. For example, some accounts may experience a temporarily lower affect when there are peripheral processes stressing the AT, when offering their AT's bandwidth to other associated accounts, or after prolonged extreme use (e.g. overheating / burnout). As stress in any form impacts the physical condition of the AT which belongs to W0, W0 as owner may pursue legal action against 3rd parties that induce AT toxicity, directly or indirectly.
+    
+    In addition, as account activity, especially when satisfied with these services, may lead to new accounts (e.g. through *de novo* creation, referral, etc). Accounts opened under an allocated AT have their AT automatically allocated and gifted.
+    
+  </div>
+</div>
+
+
+
+
 <!-- <div class="bg-slate-100 flex place-content-center">
     <div class="prose">
         {#await collaborators}
